93942ed074a94b90556deedfc789b777271d3ebb
root <root@vagrant-debian-squeeze-64.vagrantup.com>
2014-10-19 21:12:24 +0200

    more fields added

--- infoporto/FIAIP/property.py
+++ infoporto/FIAIP/property.py
@@ -27,6 +27,11 @@ class IProperty(form.Schema, IImageScaleTraversable):
     Describe a place to live or work
     """
 
+    full_description = RichText(
+            title=_(u"Descrizione estesa"),
+            required=False,
+    )
+
     id = schema.TextLine(
             title=_(u"Property ID"),
             required=False
@@ -58,9 +63,133 @@ class IProperty(form.Schema, IImageScaleTraversable):
             required=False,
     )
 
-    
-    
+    regione = schema.TextLine(
+            title=_(u"Regione"),
+            required=False,
+    )
 
+    provincia = schema.TextLine(
+            title=_(u"Provincia"),
+            required=False,
+    )
+
+    comune = schema.TextLine(
+            title=_(u"Comune"),
+            required=False,
+    )
+
+    cod_istat = schema.TextLine(
+            title=_(u"Cod. ISTAT"),
+            required=False,
+    )
+
+    localita = schema.TextLine(
+            title=_(u"Localita'"),
+            required=False,
+    )
+
+    indirizzo = schema.TextLine(
+            title=_(u"Indirizzo"),
+            required=False,
+    )
+
+    civico = schema.TextLine(
+            title=_(u"Num. civico"),
+            required=False,
+    )
+
+    tipologia = schema.TextLine(
+            title=_("Tipologia"),
+            required=False,
+    )
+
+    categoria = schema.Choice(
+            title=_("Categoria"),
+            values=[_(u'Residenziale'), _(u'Commerciale'), _(u'Rustici e terreni'),
+                    _(u'Uffici, fondi')],
+            required=False
+    )
+
+    prezzo = schema.TextLine(
+            title=_(u"Prezzo"),
+            required=False,
+    )
+
+    superficie = schema.TextLine(
+            title=_(u"Superficie"),
+            required=False,
+    )
+
+    vani = schema.TextLine(
+            title=_(u"Vani"),
+            required=False,
+    )
+
+    camere = schema.TextLine(
+            title=_(u"Camere"),
+            required=False,
+    )
+
+    bagni = schema.TextLine(
+            title=_(u"Bagni"),
+            required=False,
+    )
+
+    riscaldamento = schema.Choice(
+            title=_("Riscaldamento"),
+            values=[_(u'Non definite'), _(u'Centralizzato'), _(u'Autonomo'),
+                    _(u'Inesistente')],
+            required=False
+    )
+
+    condizioni = schema.Choice(
+            title=_("Condizioni"),
+            values=[_(u'Non definite'),_(u' Nuova costruzione'),
+                    _(u'Ristrutturato'),_(u'Da ristrutturare'),
+                    _(u'Abitabile'),_(u'Ottime'),
+                    _(u'Seminuovo'),],
+            required=False,
+    )
+
+    ascensore = schema.Bool(
+            title=_(u"Ascensore"),
+            required=False,
+    )
+
+    arredato = schema.Bool(
+            title=_(u"Arredato"),
+            required=False,
+    )
+
+    garage = schema.Bool(
+            title=_(u"Garage"),
+            required=False,
+    )
+
+    giardino = schema.Bool(
+            title=_(u"Giardino"),
+            required=False,
+    )
+
+    piano = schema.TextLine(
+            title=_(u"Piano"),
+            required=False,
+    )
+
+    classe_energ = schema.TextLine(
+            title=_(u"Classe energetica"),
+            required=False,
+    )
+
+    epi = schema.TextLine(
+            title=_(u"EPI"),
+            required=False,
+    )
+
+    epi_um = schema.TextLine(
+            title=_(u"EPI un. misura"),
+            required=False,
+    )
 
 # Custom content-type class; objects created for this content type will
 # be instances of this class. Use this class to add content-type specific
